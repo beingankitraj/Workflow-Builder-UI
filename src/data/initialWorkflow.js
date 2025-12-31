03e6f5af5cd98bf1f4083b0b2518bf3f4bd6636d
Ankit Raj <117423963+beingankitraj@users.noreply.github.com>
2025-12-31 14:12:27 +0530

    Add files via upload

---
+++ src/data/initialWorkflow.js
@@ -0,0 +1,11 @@
+export const initialWorkflow = {
+  nodes: {
+    start: {
+      id: "start",
+      type: "start",
+      label: "Start",
+      children: { main: null }
+    }
+  },
+  rootId: "start"
+};
